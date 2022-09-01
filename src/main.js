ae6775852e2b35ba1acd96230514c0cb45e8bc86
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2022-08-31 21:50:51 -0500

    Clase 4: leyendo nodos
    Utilizando 5 métodos para leer nodos del DOM en JS

---
+++ src/main.js
@@ -0,0 +1,43 @@
+//Clase 4: 
+
+function docGetElementById(item){
+    return document.getElementById(item);
+}
+
+function docGetElementsByClassName(item){
+    return document.getElementsByClassName(item);
+}
+
+function docGetElementsByTagName(item){
+    return document.getElementsByTagName(item);
+}
+
+function docQuerySelector(item){
+    return document.querySelector(item);
+}
+
+function docQuerySelectorAll(item){
+    return document.querySelectorAll(item);
+}
+
+function log(item){
+    return console.log(item);
+}
+
+log("Using id");
+log(docGetElementById('emptySpan'));
+
+log("Using class");
+log(docGetElementsByClassName('text-content'));
+
+log("Using tag");
+log(docGetElementsByTagName('p'));
+
+log("Query selector an id");
+log(docQuerySelector('#textSpan'));
+
+log("Query selector a class");
+log(docQuerySelector('.text-content'));
+
+log("Query selector all");
+log(docQuerySelectorAll('.text-content'));
